10b8c62eaf7b2ba3461d0ee0258ffc5bf267ec9f
Thomas Skerbis <thomas.skerbis@mac.com>
2019-10-09 22:46:12 +0200

    Lazy element wirft 404 bei aktuellem YRewrite (#60)
    * Lazy element wirft 404 bei aktuellem YRewrite
    
    Fix für: https://github.com/yakamara/redaxo_yrewrite/issues/298
    
    * Fix CS

--- lib/api_minibar.php
+++ lib/api_minibar.php
@@ -28,7 +28,7 @@ class rex_api_minibar extends rex_api_function
                 $fragment = new rex_fragment([
                     'element' => $element,
                 ]);
-
+                rex_response::setStatus(rex_response::HTTP_OK);
                 rex_response::sendContent($fragment->parse('core/minibar/minibar_element.php'));
                 exit();
             }
