578b825f085979a2aacc582f030463f21bfe883e
Markus Staab <markus.staab@redaxo.de>
2020-08-30 15:34:14 +0200

    fix session handling

--- lib/element/syslog.php
+++ lib/element/syslog.php
@@ -9,7 +9,8 @@ class rex_minibar_element_syslog extends rex_minibar_element
     public function __construct()
     {
         if (rex::isBackend() && rex_be_controller::getCurrentPage() == 'system/log/redaxo') {
-            rex_set_session('rex_syslog_last_seen', filemtime(rex_logger::getPath()) );
+            $login = rex::getProperty('login');
+            $login->setSessionVar('rex_syslog_last_seen', filemtime(rex_logger::getPath()) );
         }
     }
 
@@ -21,8 +22,10 @@ class rex_minibar_element_syslog extends rex_minibar_element
         // in case someone else aready read the filemtime() and the file was changed afterwards within the same request
         clearstatcache( true, $sysLogFile );
         $lastModified = filemtime($sysLogFile);
+
         // "last-seen" will be updated, when the user looks into the syslog
-        $lastSeen = rex_session('rex_syslog_last_seen');
+        $login = rex::getProperty('login');
+        $lastSeen = $login->getSessionVar('rex_syslog_last_seen');
 
         // when the user never looked into the file (e.g. after login), we dont have a timely reference point.
         // therefore we check for changes in the file within the last 24hours
