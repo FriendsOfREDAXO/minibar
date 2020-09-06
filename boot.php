91c62b5199db5f2a95ec081ebb69c2c0a1d824b6
Markus Staab <markus.staab@redaxo.de>
2020-09-06 09:53:38 +0200

    use shadow dom to scope minibar styles

--- boot.php
+++ boot.php
@@ -38,7 +38,6 @@ if (rex::isBackend()) {
 
     if (rex_minibar::getInstance()->shouldRender()) {
         rex_view::addJsFile($addon->getAssetsUrl('minibar.js'));
-        rex_view::addCssFile($addon->getAssetsUrl('styles.css'));
     }
 
     // XXX vermutlich nicht mehr nötig?
--- fragments/core/minibar/minibar_backend.php
+++ fragments/core/minibar/minibar_backend.php
@@ -1,11 +1,23 @@
-<div class="rex-minibar rex-minibar-backend">
-    <div class="rex-minibar-elements">
-        <?php
-        foreach ($this->elements as $element) {
-            $this->subfragment('core/minibar/minibar_element.php', [
-                'element' => $element,
-            ]);
-        }
-        ?>
-    </div>
-</div>
+<div class="rex-minibar rex-minibar-backend"></div>
+
+<script>
+var minibarContent = `
+<?php
+$addon = rex_addon::get('minibar');
+$styles = $addon->getAssetsUrl('styles.css').'?v='. $addon->getVersion();
+echo '<link rel="stylesheet" type="text/css" media="all" href="'. $styles .'">';
+?>
+<div class="rex-minibar-elements">
+    <?php
+    foreach ($this->elements as $element) {
+        $subFragment = $this->getSubfragment('core/minibar/minibar_element.php', [
+            'element' => $element,
+        ]);
+        echo str_replace('\\', '\\\\', $subFragment);
+    }
+    ?>
+</div>`;
+
+var shadow = document.querySelector('.rex-minibar').attachShadow({mode: 'open'});
+shadow.innerHTML = minibarContent;
+</script>
