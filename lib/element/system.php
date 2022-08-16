3be588c57f24c76f336f12af9af07d0a274baed6
Thomas Skerbis <thomas.skerbis@mac.com>
2022-08-16 18:04:00 +0200

    Update system.php

--- lib/element/system.php
+++ lib/element/system.php
@@ -14,7 +14,7 @@ class rex_minibar_element_system extends rex_minibar_element
             $links .= '<a href="https://redaxo.org/doku/master" target="_blank" rel="help noreferrer noopener">'.rex_i18n::msg('minibar_documentation_link_label').'</a>';
         }
 
-        $logo = str_replace('<svg ', '<svg class="rex-redaxo-logo" ', rex_file::get(rex_url::coreAssets('redaxo-logo.svg')));
+        $logo = str_replace('<svg ', '<svg class="rex-redaxo-logo" ', (string) rex_file::get(rex_url::coreAssets('redaxo-logo.svg')));
         return
         '<div class="rex-minibar-item">
             <span class="rex-minibar-icon">
