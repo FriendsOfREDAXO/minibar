4bfd05d3b714f0ed5549c1737cee1d46cecd4a9d
Thomas Skerbis <thomas.skerbis@mac.com>
2024-02-15 20:53:03 +0100

    Update install.php

--- install.php
+++ install.php
@@ -1,15 +1,3 @@
 <?php
 
-$addon = rex_addon::get('minibar');
 
-// kommt mit be_style addon
-if (class_exists('rex_scss_compiler')) {
-    $compiler = new rex_scss_compiler();
-
-    $compiler->setRootDir(rex_path::addon('minibar/scss'));
-    $compiler->setScssFile([$addon->getPath('scss/styles.scss')]);
-
-    // Compile in backend assets dir
-    $compiler->setCssFile($addon->getPath('assets/styles.css'));
-    $compiler->compile();
-}
