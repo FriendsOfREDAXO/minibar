6965c4f044d5185cdd6357784d5d0e88e369757c
Thomas Skerbis <thomas.skerbis@mac.com>
2024-02-15 20:59:00 +0100

    compile on first boot

--- boot.php
+++ boot.php
@@ -1,5 +1,19 @@
 <?php
 
+$addon = rex_addon::get('minibar');
+
+if (class_exists('rex_scss_compiler') && $addon->getConfig('compile')  === '1') {
+    $compiler = new rex_scss_compiler();
+
+    $compiler->setRootDir(rex_path::addon('minibar/scss'));
+    $compiler->setScssFile([$addon->getPath('scss/styles.scss')]);
+
+    // Compile in backend assets dir
+    $compiler->setCssFile($addon->getPath('assets/styles.css'));
+    $compiler->compile();
+    $addon->setConfig('compile',0)
+}
+
 $mypage = 'minibar';
 $addon = rex_addon::get('minibar');
 
